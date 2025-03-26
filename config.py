b585ae1ed9a78ced61cb1c7177e978fa1ddfb570
II_CHALCOGEN_II <166417113+CHALCOGEN645@users.noreply.github.com>
2025-03-26 21:27:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 900))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @BRANDRD_ROBOT on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", "7943244871"))
+OWNER_ID = int(getenv("OWNER_ID", "7804554129"))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/CHALCOGEN645/MUSICBOT",
+    "https://github.com/CHALCOGEN645/system",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
